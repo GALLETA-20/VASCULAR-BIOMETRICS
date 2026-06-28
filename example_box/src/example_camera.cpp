b8d90cefc32707191d275ea07bea89ece8afe3af
GALLETA-20 <conchipsgalleta@gmail.com>
2026-06-28 04:00:20 -0500

    Add documentation header to example_camera.cpp
    Added file header documentation for example_camera.cpp.

--- example_box/src/example_camera.cpp
+++ example_box/src/example_camera.cpp
@@ -1,3 +1,21 @@
+/**
+ * @file example_camera.cpp
+ * @brief Sistema de Captura Vascular - Proyecto Vessel Biometric
+ * * @author Vitaly Paolo Urbina Arévalo
+ * @date Junio 2026
+ * * Copyright (c) 2026 Vitaly Paolo Urbina Arévalo.
+ * * Este programa es software libre: usted puede redistribuirlo y/o modificarlo
+ * bajo los términos de la Licencia Pública General GNU publicada por la
+ * Free Software Foundation, ya sea la versión 3 de la Licencia, o (a su
+ * elección) cualquier versión posterior.
+ * * Este programa se distribuye con la esperanza de que sea útil, pero
+ * SIN NINGUNA GARANTÍA; ni siquiera la garantía implícita de
+ * MERCANTILIDAD o APTITUD PARA UN PROPÓSITO PARTICULAR. Consulte la
+ * Licencia Pública General GNU para más detalles.
+ * * Debería haber recibido una copia de la Licencia Pública General GNU
+ * junto con este programa. Si no, consulte <https://www.gnu.org/licenses/>.
+ */
+
 #include "CAMERA.hpp"
 #include "opencv4/opencv2/opencv.hpp"
 #include <iostream>
